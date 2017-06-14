2de4be2b50a5c4327e5ca80c7e0ef9f99658b16a
Lloyd Grubham <grubham@gmail.com>
2017-06-14 13:41:23 -0600

    Completed all functionality.

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,87 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+
+var initialState = {
+    currentValue: 0,
+    pastValues: [],
+    futureValues: []
+}
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const UNDO = 'UNDO';
+const REDO = 'REDO';
+const RESET = 'RESET';
+
+export function increment(amt) {
+    return {
+        type: INCREMENT,
+        payload: amt
+    }
+}
+export function decrement(amt) {
+    return {
+        type: DECREMENT,
+        payload: amt
+    }
+}
+export function undo() {
+    return {
+        type: UNDO
+    }
+}
+export function redo() {
+    return {
+        type: REDO
+    }
+}
+export function reset() {
+    return {
+        type: RESET
+    }
+}
+
+function reducer(state = initialState, action) {
+    state = Object.assign(state, {lastAction: action});
+    var pv = state.pastValues.slice();
+    var fv = state.futureValues.slice();
+    var temp;
+
+    switch (action.type){
+        case INCREMENT:
+            pv.push(state.currentValue);
+            return Object.assign({}, state, {
+                pastValues: pv,
+                currentValue: state.currentValue + action.payload,
+                futureValues: []
+            });
+        case DECREMENT:
+            pv.push(state.currentValue);
+            return Object.assign({}, state, {
+                pastValues: pv,                
+                currentValue: state.currentValue - action.payload,
+                futureValues: []
+            });
+        case UNDO:
+            fv.push(state.currentValue);
+            temp = pv.pop();
+            return Object.assign({}, state, {
+                pastValues: pv,
+                futureValues: fv,
+                currentValue: temp
+            });
+        case REDO:
+            pv.push(state.currentValue);
+            temp = fv.pop();
+            return Object.assign({}, state, {
+                pastValues: pv,
+                futureValues: fv,
+                currentValue: temp
+            });
+        case RESET:
+            return Object.assign({}, state, initialState);
+        default:
+            return state;
+    }
+}
+
+export default reducer;
